2d7e2e3ae9493818c5afe5c6a8025c338466f8a2
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2024-03-07 12:29:57 +0300

    precentage function corrected

--- CalculatorApplication/app/src/main/java/com/example/calculatorapplication/MainActivity.kt
+++ CalculatorApplication/app/src/main/java/com/example/calculatorapplication/MainActivity.kt
@@ -9,28 +9,29 @@ import android.widget.Toast
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var one : Button
-    private lateinit var two : Button
-    private lateinit var three : Button
-    private lateinit var four : Button
-    private lateinit var five : Button
-    private lateinit var six : Button
-    private lateinit var seven : Button
-    private lateinit var eight : Button
-    private lateinit var nine : Button
-    private lateinit var zero : Button
-    private lateinit var dot : Button
-    private lateinit var equalsSign : Button
-    private lateinit var additionSign : Button
-    private lateinit var subtractionSign : Button
-    private lateinit var divisionSign : Button
-    private lateinit var multiplicationSign : Button
-    private lateinit var percentageSign : Button
-    private lateinit var clearButton : Button
-    private lateinit var backSpace : ImageButton
+    private lateinit var one: Button
+    private lateinit var two: Button
+    private lateinit var three: Button
+    private lateinit var four: Button
+    private lateinit var five: Button
+    private lateinit var six: Button
+    private lateinit var seven: Button
+    private lateinit var eight: Button
+    private lateinit var nine: Button
+    private lateinit var zero: Button
+    private lateinit var dot: Button
+    private lateinit var equalsSign: Button
+    private lateinit var additionSign: Button
+    private lateinit var subtractionSign: Button
+    private lateinit var divisionSign: Button
+    private lateinit var multiplicationSign: Button
+    private lateinit var percentageSign: Button
+    private lateinit var clearButton: Button
+    private lateinit var backSpace: ImageButton
 
-    private lateinit var  expression : TextView
-    private lateinit var result : TextView
+    private lateinit var expression: TextView
+    private lateinit var result: TextView
+    private var lastResult: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,37 +63,37 @@ class MainActivity : AppCompatActivity() {
 
         // Set OnClickListener for the 'one' button
         one.setOnClickListener {
-            appendOnTheExpression("1",true)
+            appendOnTheExpression("1", true)
         }
         two.setOnClickListener {
-            appendOnTheExpression("2",true)
+            appendOnTheExpression("2", true)
         }
         three.setOnClickListener {
-            appendOnTheExpression("3",true)
+            appendOnTheExpression("3", true)
         }
         four.setOnClickListener {
-            appendOnTheExpression("4",true)
+            appendOnTheExpression("4", true)
         }
         five.setOnClickListener {
-            appendOnTheExpression("5",true)
+            appendOnTheExpression("5", true)
         }
         six.setOnClickListener {
-            appendOnTheExpression("6",true)
+            appendOnTheExpression("6", true)
         }
         seven.setOnClickListener {
-            appendOnTheExpression("7",true)
+            appendOnTheExpression("7", true)
         }
         eight.setOnClickListener {
-            appendOnTheExpression("8",true)
+            appendOnTheExpression("8", true)
         }
         nine.setOnClickListener {
-            appendOnTheExpression("9",true)
+            appendOnTheExpression("9", true)
         }
         zero.setOnClickListener {
-            appendOnTheExpression("0",true)
+            appendOnTheExpression("0", true)
         }
         dot.setOnClickListener {
-            appendOnTheExpression(".",true)
+            appendOnTheExpression(".", true)
         }
         equalsSign.setOnClickListener {
             calculate()
@@ -111,7 +112,7 @@ class MainActivity : AppCompatActivity() {
         }
         percentageSign.setOnClickListener {
             appendOnTheExpression("%", false)
-            calculatePercentage()
+
         }
         clearButton.setOnClickListener {
             result.text = ""
@@ -119,26 +120,49 @@ class MainActivity : AppCompatActivity() {
         }
         backSpace.setOnClickListener {
             val currentExpression = expression.text.toString()
-            if (currentExpression.isNotEmpty()){
-                expression.text = currentExpression.substring(0,currentExpression.length-1)
+            if (currentExpression.isNotEmpty()) {
+                expression.text = currentExpression.substring(0, currentExpression.length - 1)
             }
             result.text = ""
         }
     }
+
+
     private fun calculatePercentage() {
         try {
+            // Extract the expression from the expression TextView
             val inputExpression = expression.text.toString()
-            val percentageValue = inputExpression.toDouble() / 100
+
+            // Remove the percentage sign from the expression
+            val expressionWithoutPercentage = inputExpression.replace("%", "")
+
+            // Evaluate the expression
+            val expressionBuilder = ExpressionBuilder(expressionWithoutPercentage)
+            val input = expressionBuilder.build()
+            val output = input.evaluate()
+
+            // Calculate the percentage value by dividing the output by 100
+            val percentageValue = output / 100.0
+
+            // Display the percentage value
             result.text = percentageValue.toString()
-            expression.text = "" // Clear the expression TextView after percentage calculation
+
+            // Clear the expression TextView after percentage calculation
+           // expression.text = ""
         } catch (e: Exception) {
             Toast.makeText(this@MainActivity, "Invalid input", Toast.LENGTH_SHORT).show()
         }
     }
 
-
     // Append function
     private fun appendOnTheExpression(stringFromButton: String, clearStatus: Boolean) {
+
+        // Clear the expression view and append the last result
+        if (!clearStatus && lastResult.isNotEmpty()) {
+            expression.text = lastResult
+            lastResult = ""
+        }
+
         if (clearStatus) {
             result.text = ""
             // Check if the expression is '0' and the input is not a dot
@@ -154,24 +178,34 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun clear(){
+    private fun clear() {
         expression.text = ""
         result.text = ""
     }
-    private fun calculate(){
+    private fun calculate() {
         try {
-            val input = ExpressionBuilder(expression.text.toString()).build()
-            val output = input.evaluate()
-            val longResult = output.toLong()
+            val inputExpression = expression.text.toString()
 
-            if (output == longResult.toDouble()) {
-                result.text = longResult.toString()
-            }else{
-                result.text = result.toString()
+            // Check if the expression contains a percentage sign
+            if (inputExpression.contains("%")) {
+                calculatePercentage()
+            } else {
+                // Evaluate the expression as usual
+                val input = ExpressionBuilder(inputExpression).build()
+                val output = input.evaluate()
 
+                // Check if the output is a whole number
+                if (output == output.toLong().toDouble()) {
+                    // Display whole number as integer
+                    result.text = output.toLong().toString()
+                } else {
+                    // Display decimal number with decimal points
+                    result.text = output.toString()
+                }
             }
-        }catch (e:Exception){
-           Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
         }
     }
+
 }
