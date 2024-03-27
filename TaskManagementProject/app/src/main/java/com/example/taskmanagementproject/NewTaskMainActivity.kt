2d055d3b81b8f225f7c7a9eefc994968ffc3cdd9
DerrickLegacy <ahaabwederrick67@gmail.com>
2024-03-27 16:42:01 +0300

    Final stages of create new tash page

--- TaskManagementProject/app/src/main/java/com/example/taskmanagementproject/NewTaskMainActivity.kt
+++ TaskManagementProject/app/src/main/java/com/example/taskmanagementproject/NewTaskMainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.taskmanagementproject
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
-class MainActivity : AppCompatActivity() {
+lateinit var spinnerPriority :Spinner
+class NewTaskMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.new_task_activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        spinnerPriority = findViewById(R.id.spinner_priority)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.priorities,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerPriority.adapter = adapter
+//        spinnerPriority.setBackgroundColor(Color.WHITE)
     }
 }
